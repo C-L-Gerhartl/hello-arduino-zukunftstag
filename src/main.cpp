849da46cacb94ea27d42955f6cacb53707927fc3
ChristianSteinG <christian.stein@gerhartl.de>
2026-04-22 14:52:54 +0200

    PlatformIO.ini mod

--- src/main.cpp
+++ src/main.cpp
@@ -13,30 +13,6 @@ byte FADE_SCHRITT = 5;          // Bestimmt die Geschwindigkeit des „fadens“
 /* Variablen deklarieren */
 byte helligkeit = 0;            // Das Wort „Helligkeit“ steht nun für den Wert, der bei der PWM ausgegeben wird
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LEDRED_PIN, OUTPUT);     // Der Pin mit den roten LEDs (Pin11) ist ein Ausgang
-  pinMode(LEDBLUE_PIN, OUTPUT);    // Der Pin mit den blauen LEDs (Pin9) ist ein Ausgang
-  pinMode(LEDGREEN_PIN, OUTPUT);   // Der Pin mit den grünen LEDs (Pin10) ist ein Ausgang
-  pinMode(SWITCH01, INPUT_PULLUP); // Der Pin mit dem Schalter "SWITCH01" (Pin2) ist ein Eingang mit Pullup Widerstand
-  pinMode(SWITCH02, INPUT_PULLUP); // Der Pin mit dem Schalter "SWITCH02" (Pin4) ist ein Eingang mit Pullup Widerstand
-}
-
-void loop() {
-  if ((digitalRead(SWITCH01) == HIGH) && (digitalRead(SWITCH02) == LOW)) {
-    Blinken();
-    Serial.println("blinken...");
-  }
-  else if ((digitalRead(SWITCH02) == HIGH) && (digitalRead(SWITCH01) == LOW)) {
-    Dimmen();
-    Serial.println("dimmen...");
-  }
-  else {
-    Licht();
-    Serial.println("an...");
-  }  
-}
-
 void Dimmen() { 
   analogWrite(LEDRED_PIN, helligkeit);       // LED (Pin9) die PWM Ausgabe aktiviert
   analogWrite(LEDBLUE_PIN, helligkeit);      // LED (Pin9) die PWM Ausgabe aktiviert
@@ -65,4 +41,28 @@ void Licht() {
   digitalWrite(LEDBLUE_PIN, HIGH);
   digitalWrite(LEDGREEN_PIN, HIGH);
   delay(20);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LEDRED_PIN, OUTPUT);     // Der Pin mit den roten LEDs (Pin11) ist ein Ausgang
+  pinMode(LEDBLUE_PIN, OUTPUT);    // Der Pin mit den blauen LEDs (Pin9) ist ein Ausgang
+  pinMode(LEDGREEN_PIN, OUTPUT);   // Der Pin mit den grünen LEDs (Pin10) ist ein Ausgang
+  pinMode(SWITCH01, INPUT_PULLUP); // Der Pin mit dem Schalter "SWITCH01" (Pin2) ist ein Eingang mit Pullup Widerstand
+  pinMode(SWITCH02, INPUT_PULLUP); // Der Pin mit dem Schalter "SWITCH02" (Pin4) ist ein Eingang mit Pullup Widerstand
+}
+
+void loop() {
+  if ((digitalRead(SWITCH01) == HIGH) && (digitalRead(SWITCH02) == LOW)) {
+    Blinken();
+    Serial.println("blinken...");
+  }
+  else if ((digitalRead(SWITCH02) == HIGH) && (digitalRead(SWITCH01) == LOW)) {
+    Dimmen();
+    Serial.println("dimmen...");
+  }
+  else {
+    Licht();
+    Serial.println("an...");
+  }  
 }
